ab0b0a2be5b1c00b08b805900a33b2b0ac0cb6fe
AdivkSaxena1 <advik.1saxena.1@gmail.com>
2024-10-05 13:03:05 +0530

    Update 16.py
    CodeBots
    lang: Python
    Q-16

--- Python/Easy/16.py
+++ Python/Easy/16.py
@@ -1,5 +1,12 @@
+l1,l2=[],[]
 L=(1,2,3,4)
-L.append(5)
+for i in L:
+    l1.append(i)
+l1.append(5)
 my_tuple = (1, 2, 3)
-
-my_tuple.append(4)  # 'tuple' object has no 'append' method
+for i in my_tuple:
+    l2.append(i)
+l2.append(5)
+  # 'tuple' object has no 'append' method
+print(tuple(l1))
+print(tuple(l2))
